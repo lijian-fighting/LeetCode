e6a852591faf0f1810b1915b1257e0d5830479d1
lijian-fighting <1063861655@qq.com>
2018-03-12 10:37:16 +0800

    135

---
+++ question135.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+/**
+ * @Author: LiJian
+ * @Description:
+ * @Date: Created in 10:04 2018/3/12
+ * @Modified By:
+ */
+public class question135 {
+    //一分为二考虑，从左往右，从右往左
+    public int candy(int[] ratings) {
+        int sum = 0;
+        int[] left2right = new int[ratings.length];
+        int[] right2left = new int[ratings.length];
+        Arrays.fill(left2right, 1);
+        Arrays.fill(right2left, 1);
+        for (int i = 1; i < ratings.length; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                left2right[i] = left2right[i - 1] + 1;
+            }
+        }
+        for (int i = ratings.length - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
+                right2left[i] = right2left[i + 1] + 1;
+            }
+        }
+        for (int i = 0; i < ratings.length; i++) {
+            sum += Math.max(left2right[i], right2left[i]);
+        }
+        return sum;
+    }
+}
