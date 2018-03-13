2d4b4f442e4f7d3c99670fb99db2b5bc0c2cc6b1
lijian-fighting <1063861655@qq.com>
2018-03-13 15:08:14 +0800

    question75

---
+++ question75.java
@@ -0,0 +1,25 @@
+/**
+ * @Author: LiJian
+ * @Description:
+ * @Date: Created in 14:43 2018/3/13
+ * @Modified By:
+ */
+public class question75 {
+    public void sortColors(int[] nums) {
+        int p1 = 0, p2 = nums.length - 1, index = 0;
+        while (index <= p2) {
+            if (nums[index] == 0) {
+                nums[index] = nums[p1];
+                nums[p1] = 0;
+                p1++;
+            }
+            if (nums[index] == 2) {
+                nums[index] = nums[p2];
+                nums[p2] = 2;
+                p2--;
+                index--;
+            }
+            index++;
+        }
+    }
+}
