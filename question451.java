86f76e8dfcc82b741b109a5783bf688688cf3edb
lijian-fighting <1063861655@qq.com>
2018-03-20 12:37:59 +0800

    map+bucket sort

---
+++ question451.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+/**
+ * @Author: LiJian
+ * @Description:
+ * @Date: Created in 11:13 2018/3/20
+ * @Modified By:
+ */
+public class question451 {
+    public String frequencySort(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            if (map.containsKey(c)) {
+                map.put(c, map.get(c) + 1);
+            } else {
+                map.put(c, 1);
+            }
+        }
+        List<Character> [] bucket = new List[s.length() + 1];
+        for (char key : map.keySet()) {
+            int frequency = map.get(key);
+            if (bucket[frequency] == null) {
+                bucket[frequency] = new ArrayList<>();
+            }
+            bucket[frequency].add(key);
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int pos = bucket.length - 1; pos >=0; pos--) {
+            if (bucket[pos] != null) {
+                for (char num : bucket[pos]) {
+                    for (int i = 0; i < map.get(num); i++) {
+                        sb.append(num);
+                    }
+                }
+            }
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        question451 q = new question451();
+        System.out.println(q.frequencySort("Aabb"));
+    }
+}
