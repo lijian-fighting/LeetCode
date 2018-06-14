b9fa4c86dba89271ae4c5c12f8aa3a2170c76d16
lijian-fighting <1063861655@qq.com>
2018-06-14 12:13:37 +0800

    二分查找,left为最大值，right为和

---
+++ question410.java
@@ -0,0 +1,43 @@
+/**
+ * @Author: LiJian
+ * @Description:
+ * @Date: Created in 10:54 2018/6/14
+ * @Modified By:
+ */
+public class question410 {
+    //二分查找
+    public int splitArray(int[] nums, int m) {
+        int max = 0; long sum = 0;
+        for (int num : nums) {
+            max = Math.max(num, max);
+            sum += num;
+        }
+        if (m == 1) return (int)sum;
+        //binary search
+        long l = max; long r = sum;
+        while (l <= r) {
+            long mid = (l + r)/ 2;
+            if (valid(mid, nums, m)) {
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return (int)l;
+    }
+    public boolean valid(long target, int[] nums, int m) {
+        int count = 1;
+        long total = 0;
+        for(int num : nums) {
+            total += num;
+            if (total > target) {
+                total = num;
+                count++;
+                if (count > m) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
