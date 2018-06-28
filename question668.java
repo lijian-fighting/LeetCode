56dde7d121f8be2806419dd0028d724f55566b5b
lijian-fighting <1063861655@qq.com>
2018-06-28 10:27:02 +0800

    二分搜索,求出中间值，看比这个中间值小的所有数是否满足k个，直到无法寻找了

---
+++ question668.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+/**
+ * @Author: LiJian
+ * @Description:
+ * @Date: Created in 9:59 2018/6/28
+ * @Modified By:
+ */
+public class question668 {
+    public boolean enough(int x, int m, int n, int k) {
+        int count = 0;
+        for (int i = 1; i <= m; i++) {
+            count += Math.min(x / i, n);
+        }
+        return count >= k;
+    }
+
+    public int findKthNumber(int m, int n, int k) {
+        int lo = 1, hi = m * n;
+        while (lo < hi) {
+            int mi = lo + (hi - lo) / 2;
+            if (!enough(mi, m, n, k)) lo = mi + 1;
+            else hi = mi;
+        }
+        return lo;
+    }
+
+    public static void main(String[] args) {
+        question668 q = new question668();
+        System.out.println(q.findKthNumber(3,3,5));
+    }
+}
