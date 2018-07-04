5dedd7f679e672fb4922c7135ceb748102498baa
lijian-fighting <1063861655@qq.com>
2018-07-04 10:46:14 +0800

    Dp

---
+++ question740.java
@@ -0,0 +1,30 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Author: LiJian
+ * @Description:
+ * @Date: Created in 10:32 2018/7/4
+ * @Modified By:
+ */
+public class question740 {
+    public int deleteAndEarn(int[] nums) {
+        int[] count = new int[10001];
+        for (int x: nums) count[x]++;
+        int avoid = 0, using = 0, prev = -1;
+
+        for (int k = 0; k <= 10000; ++k) if (count[k] > 0) {
+            int m = Math.max(avoid, using);
+            if (k - 1 != prev) {
+                using = k * count[k] + m;
+                avoid = m;
+            } else {
+                using = k * count[k] + avoid;
+                avoid = m;
+            }
+            prev = k;
+        }
+        return Math.max(avoid, using);
+
+    }
+}
