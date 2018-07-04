9598d4f984712a88cb3711486a3841b5bb3c59dd
lijian-fighting <1063861655@qq.com>
2018-07-04 10:04:05 +0800

    判断两种情况，A等于B，是否有相同字母或者A不等于B，是否可以swap

---
+++ question859.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * @Author: LiJian
+ * @Description:
+ * @Date: Created in 9:53 2018/7/4
+ * @Modified By:
+ */
+public class question859 {
+    public boolean buddyStrings(String A, String B) {
+        if (A.length() != B.length()) return false;
+        if (A.equals(B)) {
+            Set<Character> s = new HashSet<Character>();
+            for (char c : A.toCharArray()) s.add(c);
+            return s.size() < A.length();
+        }
+        List<Integer> dif = new ArrayList<>();
+        for (int i = 0; i < A.length(); ++i) if (A.charAt(i) != B.charAt(i)) dif.add(i);
+        return dif.size() == 2 && A.charAt(dif.get(0)) == B.charAt(dif.get(1)) && A.charAt(dif.get(1)) == B.charAt(dif.get(0));
+    }
+}
